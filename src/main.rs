14cd453498700e8db83a1b5ec970c52598d2fa17
jCabala <kuba.cabala6@gmail.com>
2026-06-14 19:30:06 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,65 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+    if let Err(err) = run() {
+        println!("server error: {}", err);
+    }
+}
+
+fn run() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                if let Err(err) = _stream.write("+PONG\r\n".as_bytes()) {
-                    print!("write err: {}", err);
+            Ok(mut stream) => {
+                loop {
+                    match run_request(&mut stream) {
+                        Ok(false) => {
+                            // Client closed the connection.
+                            break;
+                        }
+                        Ok(true) => {
+                            // Handled one request, keep waiting for more.
+                        }
+                        Err(err) => {
+                            println!("connection error: {}", err);
+                            break;
+                        }
+                    }
                 }
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(err) => {
+                println!("accept error: {}", err);
             }
         }
     }
+
+    Ok(())
+}
+
+fn run_request(stream: &mut TcpStream) -> std::io::Result<bool> {
+    let mut buffer = [0; 512];
+
+    let size = stream.read(&mut buffer)?;
+
+    if size == 0 {
+        return Ok(false);
+    }
+
+    println!(
+        "Received {} bytes: {:?}",
+        size,
+        String::from_utf8_lossy(&buffer[..size])
+    );
+
+    stream.write_all("+PONG\r\n".as_bytes())?;
+
+    Ok(true)
 }
